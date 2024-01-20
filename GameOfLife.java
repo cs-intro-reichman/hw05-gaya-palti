83dc5c70f9f7e649f2718c71302155a27cdf403f
gaya-palti <gaya.palti@post.runi.ac.il>
2024-01-20 22:44:11 +0200

    change spaces

--- GameOfLife.java
+++ GameOfLife.java
@@ -139,9 +139,9 @@ public class GameOfLife {
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     public static void print(int[][] arr) {
 		for (int i = 1; i < arr.length - 1; i++){
-			System.out.print(" ");
+			System.out.print("  ");
 			for (int j = 1; j < arr[i].length - 1; j++){
-				System.out.print(arr[i][j] + " ");
+				System.out.print(arr[i][j] + "  ");
 			}
 			System.out.println();
 		}
